2894e57d1fe9469f068acccdbf8806798602d872
Christian Becher <phaziz@gmail.com>
2021-05-29 19:20:28 +0200

    Update index.php
    Frühjahrsputz

--- mr-render/index.php
+++ mr-render/index.php
@@ -15,156 +15,134 @@
         TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
         0. YOU JUST DO WHAT THE FUCK YOU WANT TO!
 
-        +++ Visit http://phaziz.com +++
-        +++ Visit http://blog.phaziz.com +++
+        +++ Visit https://github.com/phaziz +++
 
     ***************************************************************************
     */
 
-    /*namespacing*/
-	namespace App;
-	/*namespacing*/
 
-	/*Defining the routes for your website*/
+	namespace MrRender;
+
     require_once 'routes.php';
-	/*Defining the routes for your website*/
 
-	/*MrRender*/
 	class MrRender
 	{
-		const _BASE_URL 		= 'http://demo.phaziz.com';
+		const _BASE_URL 		= 'http://localhost';
 		const _BASE_TITLE 		= 'Welcome!';
 		const _TPL_DIRECTORY 	= '/content';
+		const _USE_CACHE 		= false;
 		const _CACHE_DIRECTORY 	= '/cache/';
 		const _CACHE_TIME 		= 86400;
 		const _CACHE_FILEENDING = '.html';
-		const _CACHE_DELETR     = 'ThisIsAFuckingUniqueAndAlsoTopSecretStringToCleanTheCacheDirectory';
-		const _AFTER_CACHE_DEL	= 'http://demo.phaziz.com/mini-cms-4';
-		const _CDN_LINK 		= 'http://demo.phaziz.com/mini-cms-4/content/';
+		const _CDN_LINK 		= 'http://localhost/mr-render/content/';
 		const _MAIN_TPL 		= 'startseite.php';
 		const _404_TPL 			= '404.php';
 		const _ERROR_TPL 		= 'error.php';
 
-		function renderTPL($ROUTES_ARRAY)
+		function __construct($ROUTES_ARRAY)
 		{
-			if(isset($_GET['del-cache']) && $_GET['del-cache'] == self::_CACHE_DELETR)
-			{
-				self::cleanCacheDirectory();
-				@header('Location: ' . self::_AFTER_CACHE_DEL);
-			}
-
 			$REQUEST = $_SERVER['REQUEST_URI'];
-			$UNIQUE = __DIR__ . self::_CACHE_DIRECTORY . md5($REQUEST) . self::_CACHE_FILEENDING;
+			$UNIQUE = __DIR__ . self::_CACHE_DIRECTORY . base64_encode($REQUEST) . self::_CACHE_FILEENDING;
 
-			if(file_exists($UNIQUE) && time() - filemtime($UNIQUE) <= self::_CACHE_TIME)
-			{
-				$TPL_STR = @file_get_contents(__DIR__ . self::_CACHE_DIRECTORY . md5($REQUEST) . self::_CACHE_FILEENDING);
+			if(!!self::_USE_CACHE && file_exists($UNIQUE) && time() - filemtime($UNIQUE) <= self::_CACHE_TIME){
+				$TPL_STR = @file_get_contents($UNIQUE);
 				echo $TPL_STR;
+
 				die();
-			}
-			else
-			{
-				$FLATTEN = self::flatten($ROUTES_ARRAY);
+			} else {
+				$FLATTEN = $this->flatten($ROUTES_ARRAY);
 
-				if($REQUEST == '' || $REQUEST == '/')
-				{
+				if('' === $REQUEST || '/' === $REQUEST){
 					$TPL_STR = @file_get_contents(__DIR__ . self::_TPL_DIRECTORY . DIRECTORY_SEPARATOR . self::_MAIN_TPL);
-					$TPL_STR = str_replace('{@ printNavigation @}',self::recursiveNavigation($ROUTES_ARRAY,0),$TPL_STR);
-					$TPL_STR = str_replace('{@ printPageName @}',self::_BASE_TITLE,$TPL_STR);
-					$TPL_STR = str_replace('{@ printCDNLink @}',self::_CDN_LINK,$TPL_STR);
-					$NEW_CACHE = $TPL_STR;
-					@file_put_contents($UNIQUE,$NEW_CACHE . "\n" . '<!-- MrRender Cache ' . date('Y-m-d, H:i:s') . ' | http://phaziz.com | http://blog.phaziz.com -->');
+					$TPL_STR = str_replace('{@ printNavigation @}', $this->navigation($ROUTES_ARRAY,0),$TPL_STR);
+					$TPL_STR = str_replace('{@ printPageName @}', self::_BASE_TITLE,$TPL_STR);
+					$TPL_STR = str_replace('{@ printCDNLink @}', self::_CDN_LINK,$TPL_STR);
+					$NEW_CACHE = $TPL_STR . "\n" . '<!-- MrRender Cache ' . date('Y-m-d, H:i:s') . ' | https://github.com/phaziz -->';
+
+					if(!!self::_USE_CACHE ){
+						@file_put_contents($UNIQUE, $NEW_CACHE, LOCK_EX);
+					}
+					
 					echo $NEW_CACHE;
+				
 					die();
-				}
-				else
-				{
-					if(array_search($REQUEST,$FLATTEN) != '')
-					{
+				} else {
+					if('' !== array_search($REQUEST,$FLATTEN)){
 						$TPL_STR = @file_get_contents(__DIR__ . self::_TPL_DIRECTORY . DIRECTORY_SEPARATOR . $FLATTEN[(array_search($REQUEST,$FLATTEN) + 1)]);
-						$TPL_STR = str_replace('{@ printNavigation @}',self::recursiveNavigation($ROUTES_ARRAY,0),$TPL_STR);
-						$TPL_STR = str_replace('{@ printPageName @}',$FLATTEN[(array_search($REQUEST,$FLATTEN) - 1)],$TPL_STR);
-						$TPL_STR = str_replace('{@ printCDNLink @}',self::_CDN_LINK,$TPL_STR);
-						$NEW_CACHE = $TPL_STR;
-						@file_put_contents($UNIQUE,$NEW_CACHE . "\n" . '<!-- MrRender Cache ' . date('Y-m-d, H:i:s') . ' | http://phaziz.com | http://blog.phaziz.com -->');
-						echo $NEW_CACHE;
-						die();
-					}
-					else
-					{
-						$TPL_STR = @file_get_contents(__DIR__ . self::_TPL_DIRECTORY . DIRECTORY_SEPARATOR . self::_404_TPL);
-						$TPL_STR = str_replace('{@ printNavigation @}',self::recursiveNavigation($ROUTES_ARRAY,0),$TPL_STR);
-						$TPL_STR = str_replace('{@ printPageName @}','404 - not found',$TPL_STR);
-						$TPL_STR = str_replace('{@ printCDNLink @}',self::_CDN_LINK,$TPL_STR);
-						echo $TPL_STR;
-						die();
-					}
-				}
-			}
-		}
+						$TPL_STR = str_replace([
+							'{@ printNavigation @}', 
+							'{@ printPageName @}',
+							'{@ printCDNLink @}'
+						], [
+							$this->navigation($ROUTES_ARRAY,0), 
+							$FLATTEN[(array_search($REQUEST, $FLATTEN) - 1)],
+							self::_CDN_LINK
+						], $TPL_STR);
 
-		private function cleanCacheDirectory()
-		{
-			$C_DIRECTORY = __DIR__ . self::_CACHE_DIRECTORY;
+						$NEW_CACHE = $TPL_STR . "\n" . '<!-- MrRender Cache ' . date('Y-m-d, H:i:s') . ' | https://github.com/phaziz -->';
 
-			if(@is_dir($C_DIRECTORY))
-			{
-				if ($H = @opendir($C_DIRECTORY))
-				{
-					while (($F = @readdir($H)) !== false)
-					{
-						if ($F != '.' && $F != '..')
-						{
-							@unlink($C_DIRECTORY . $F);
+						if (!!self::_USE_CACHE) {
+							@file_put_contents($UNIQUE, $NEW_CACHE, LOCK_EX);
 						}
-					}
+						
+						echo $NEW_CACHE;
+				
+						die();
+					} else {
+						$TPL_STR = @file_get_contents(__DIR__ . self::_TPL_DIRECTORY . DIRECTORY_SEPARATOR . self::_404_TPL);
+						$TPL_STR = str_replace([
+							'{@ printNavigation @}',
+							'{@ printPageName @}',
+							'{@ Request @}',
+							'{@ Unique @}',
+							'{@ printCDNLink @}'
+						], [
+							$this->navigation($ROUTES_ARRAY,0),
+							'404 - not found',
+							$REQUEST,
+							$UNIQUE,
+							self::_CDN_LINK
+						], $TPL_STR);
 
-					@closedir($H);
+						echo $TPL_STR . "\n" . '<!-- MrRender Cache ' . date('Y-m-d, H:i:s') . ' | https://github.com/phaziz -->';
+
+						die();
+					}
 				}
 			}
 		}
 
-		private function flatten($ARRAY)
+		private function flatten($ARRAY) : array
 		{
-		    if (!is_array($ARRAY))
-		    {
+		    if (!is_array($ARRAY)){
 		        return array($ARRAY);
 		    }
 
 		    $RESULT = array();
 
-		    foreach ($ARRAY as $VALUE)
-		    {
-		        $RESULT = array_merge($RESULT, self::flatten($VALUE));
+		    foreach ($ARRAY as $VALUE){
+		        $RESULT = array_merge($RESULT, $this->flatten($VALUE));
 		    }
 
 		    return $RESULT;
 		}
 
-		private function recursiveNavigation($ARRAY,$LEVEL)
+		private function navigation($ARRAY,$LEVEL) : string
 		{
-			if(!empty($ARRAY))
-			{
-				if ($LEVEL > 0)
-				{
+			if(!empty($ARRAY)){
+				if ($LEVEL > 0){
 					$RET = '<ul class="dropdown-menu multi-level" role="menu">';
-				}
-				else
-				{
+				} else {
 					$RET = '<ul class="nav navbar-nav">';
 				}
 
-				foreach($ARRAY as $KEY => $VALUE)
-				{
-					if(!is_int($KEY))
-					{
+				foreach($ARRAY as $KEY => $VALUE){
+					if(!is_int($KEY)){
 						$RET .=  '<li class="dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">' . $KEY . '</a>';
 						$LEVEL++;
-						$RET .= self::recursiveNavigation($VALUE,$LEVEL);
+						$RET .= $this->navigation($VALUE,$LEVEL);
 						$RET .=  '</li>';
-					}
-					else
-					{
+					} else {
 						$RET .= '<li><a href="' . self::_BASE_URL . $VALUE['_URL'] . '">' . $VALUE['_WHAT'] . '</a></li>';
 					}
 				}
@@ -172,14 +150,11 @@
 				$RET .= '</ul>';
 
 				return $RET;
-			}
-			else
-			{
-				return false;
+			} else {
+				return '';
 			}
 		}
 	}
-	/*MrRender*/
 
-	$APP = new \App\MrRender();
-	echo $APP -> renderTPL($ROUTES_ARRAY);
+	$APP = new \MrRender\MrRender($ROUTES_ARRAY);
+	echo $APP;
